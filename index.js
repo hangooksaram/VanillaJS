e90695a609db2280e7fa9cdd41edb3a264552082
hangooksaram <hyunjae1995@gmail.com>
2020-05-01 23:38:33 +0900

    yes

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log(console)
+//console은 object, log는 function
+
+function sayHello(hello, number){
+    console.log("Hello", hello, number)
+}
+
+sayHello("Nicolas", 15);
+
+const daysOfWeek = [
+    {name : "dd",
+    age : "ss"},
+    {name : "dd",
+    age : "ss"},
+    {name : "dd",
+    age : "ss"},
+    {name : "dd",
+    age : "ss"},
+    {name : "dd",
+    age : "ss"},
+    
+]
